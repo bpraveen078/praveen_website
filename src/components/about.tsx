57c684e71ead55843b743856280c6c594141511d
Praveen <bpraveendev@gmail.com>
2019-12-25 00:32:24 +0530

    about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -16,10 +16,56 @@ export const About: React.FC = (props: IAppProps) => {
         <div className="col-sm-8">
           <div className="about-me">
             <h2>About Me</h2>
-            <h4>I'm Praveen Kumar Banda,</h4>
+            <h5>I'm Praveen Kumar Banda,</h5>
+
+            <div className="content-color-gray">
+              <div>I'm full-stack developer,</div>
+              <div>
+                For the past 5+ years, I've been developing applications using
+                .NET & front end technologies.
+              </div>
+              <div>
+                I spend a lot of time learning new techniques and actively help
+                other people learn web development through a variety of help
+                groups and writing web development tutorials for my website and
+                blog about advancements in web design and development.
+              </div>
+              <div>
+                Besides programming I love spending time with friends and family
+                and can often be found together going out catching the latest
+                movie.
+              </div>
+            </div>
+          </div>
+          <br />
+          <br />
+          <h2>Contact Details</h2>
+          <div className="row content-color-gray">
+            <div className="col-sm-4">
+              <span className="span-about-contact-label">Name</span>
+              : Praveen Kumar Banda
+              <br />
+              <span className="span-about-contact-label">Area</span>
+              : Madhapur,
+              <br />
+              <span className="span-about-contact-label">City</span>
+              : Hyderabad
+              <br />
+              <span className="span-about-contact-label">Postal Code</span>:
+              500081
+              <br />
+              <span className="span-about-contact-label">Email</span>:
+              praveen@pbanda.in
+            </div>
+            <div className="col-sm-3">
+              {" "}
+              <a className="button-download">
+                <i className="fa fa-download"></i> Download Resume
+              </a>
+            </div>
           </div>
         </div>
-        <div className="col-sm-1">aa</div>
+        <div className="col-sm-1"></div>
       </div>
     </div>
   );
